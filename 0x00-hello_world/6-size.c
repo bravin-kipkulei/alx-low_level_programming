606b8455ed8d7963c71ea124b070cb675096c5d6
smartbravin <bravinkipkulei@gmail.com>
2023-03-20 15:53:26 -0700

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main Entry - point
+  *Return 0: always (succes)
+  */
+int main(void)
+{
+	printf("size of char: %lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of long int: %lu byte(s)"), sizeof(long int);
+	printf("size of long long int: %lu byte(s)"), sizeof(long long int);
+	printf("size of float: %lu byte(s)"), sizeof(float);
+	return (0);
+}
